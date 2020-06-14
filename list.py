0a89e6cbd870344768ece92f56e5e30d55265ca2
Sae Woo Nam <nams@nist.gov>
2020-06-14 14:46:12 -0600

    Tweak list.py

--- list.py
+++ list.py
@@ -19,24 +19,28 @@ async def run():
     devices = await discover()
     addresses = []
     for d in devices:
-        if d.metadata['uuids'] is not None:
+        # print(d.__dict__.keys())
+        # print(d.address)
+        # print(d.details)
+        if 'uuids' in d.metadata:
+            # print(d.metadata['uuids'])
             if service_uuid in d.metadata['uuids']:
-                print(d.__dict__.keys())
+                print(d.name)
                 addresses.append(d.address)
-
-    # async with BleakClient(address, loop=loop) as client:
-    address = addresses[0]
-    for address in addresses:
-        print('*'*80)
-        print('address', address)
-        async with BleakClient(address) as client:
-            x = await client.is_connected()
-            logger.info("Connected: {0}".format(x))
-
-            c = await client.read_gatt_char(rw_uuid)
-            print('c: ',c)
-            count = await client.read_gatt_char(count_uuid)
-            print('count', count, int.from_bytes(bytes(count), byteorder='little'))
+    #
+    # # async with BleakClient(address, loop=loop) as client:
+    # address = addresses[0]
+    # for address in addresses:
+    #     print('*'*80)
+    #     print('address', address)
+    #     async with BleakClient(address) as client:
+    #         x = await client.is_connected()
+    #         logger.info("Connected: {0}".format(x))
+    #
+    #         c = await client.read_gatt_char(rw_uuid)
+    #         print('c: ',c)
+    #         count = await client.read_gatt_char(count_uuid)
+    #         print('count', count, int.from_bytes(bytes(count), byteorder='little'))
             
 service_uuid = '7b183224-9168-443e-a927-7aeea07e8105'
 count_uuid = '292bd3d2-14ff-45ed-9343-55d125edb721'
